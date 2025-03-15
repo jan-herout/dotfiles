008fd6f0b3fe2bde0144509de53d6aefa0259065
Jan Herout <jan.herout@o2.cz>
2025-03-15 15:34:31 +0100

    dorovnávací commit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -34,8 +34,9 @@ require("lazy").setup({
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = { enabled = false, notify = true, frequency = 604800},
 })
+
 
 --  keymaps
 require("config.colors")
--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -23,15 +23,3 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- Setup lazy.nvim
-require("lazy").setup({
-  spec = {
-    -- import your plugins
-    { import = "plugins" },
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates - once a week (frequency is in seconds)
-  checker = { enabled = false, notify = true, frequency = 604800},
-})
