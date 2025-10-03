8876f3e36dbcb984ac5542df2b79acbf668d1e1a
Jan Herout <jan.herout@gmail.com>
2025-10-03 10:51:05 +0200

    update

--- bin/cyberark.sh
+++ bin/cyberark.sh
@@ -1,29 +1,12 @@
 #!/bin/bash
-username=$(grep username "$1" | tr -d "
-" | awk 'BEGIN {FS=":"}{print $3}')
 
-address=$(grep address "$1" | tr -d "
-" | awk 'BEGIN {FS=":"}{print $3}')
+# get the first parameter
+cyberark_rdp_file=$1
+[ -z "$cyberark_rdp_file" ] && echo "Usage: $0 <rdp file.rdp>" && exit 1
 
-port=$(grep "server port" "$1" | tr -d "
-" | awk 'BEGIN {FS=":"}{print $3}')
+# call xfreerdp with the provided rdp file
+xfreerdp "$cyberark_rdp_file"
 
-width=$(grep desktopwidth "$1" | tr -d "
-" | awk 'BEGIN {FS=":"}{print $3}')
-
-height=$(grep desktopheight "$1" | tr -d "
-" | awk 'BEGIN {FS=":"}{print $3}')
-
-local_username=$(grep username "$1" | tr -d "
-" | awk 'BEGIN {FS=":"}{print $3}')
-
-echo "username=$username"
-echo "address=$address"
-echo "port=$port"
-echo "width=$width"
-echo "height=$height"
-
-echo flatpak run com.freerdp.FreeRDP /v:$address /p:$port  /shell:"PSM@3b713e7d-3057-44fb-851d-d8672080354a"
-
-# echo "rdp=xfreerdp /v:$address /p:$port /w:$width /h:$height /u:$username /cert-ignore /sec:tls"
-#rdp=`xfreerdp /v:$address /p:$port /w:$width /h:$height /u:$username /cert-ignore /sec:tls`
+# remove the file after
+notify-send "Removing: $cyberark_rdp_file" --icon=dialog-information
+rm "$cyberark_rdp_file"
