39b8753ea9b00b63374d67b1862f40942245ded7
Jan Herout <jan.herout@gmail.com>
2025-10-17 08:36:52 +0200

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -41,33 +41,3 @@ vim.schedule(function()
   require "mappings"
 end)
 
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
-
-vim.keymap.set("v", "<", "<gv", { noremap = true, silent = true })  -- Dedent
-vim.keymap.set("v", ">", ">gv", { noremap = true, silent = true })  -- Indent
--- page up, page down, center screen (zz -> center screen, noremap - stop recursion
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true }) -- Ctrl+u - up and zz
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true }) -- Ctrl+d - down and zz
-vim.keymap.set("n", "n", "nzz", { noremap = true }) -- search for next centers screen
-vim.keymap.set("n", "N", "Nzz", { noremap = true })-- search for next centers screen
--- move between splits
-vim.keymap.set('n', '<C-j>', '<C-w>j', opts)  -- Move down
-vim.keymap.set('n', '<C-k>', '<C-w>k', opts)  -- Move up
-vim.keymap.set('n', '<C-h>', '<C-w>h', opts)  -- Move left
-vim.keymap.set('n', '<C-l>', '<C-w>l', opts)  -- Move right
--- delete, do not append to register
---vim.keymap.set("n", "D", "dd")
-vim.keymap.set("n", "D", [["_dd]])
-vim.keymap.set("v", "D", [["_dd]])
--- resize splits
-vim.keymap.set('n', '<C-Up>', ':resize +4<CR>', opts)    -- Increase height
-vim.keymap.set('n', '<C-Down>', ':resize -4<CR>', opts)  -- Decrease height
-vim.keymap.set('n', '<C-Left>', ':vertical resize -4<CR>', opts)  -- Decrease width
-vim.keymap.set('n', '<C-Right>', ':vertical resize +4<CR>', opts) -- Increase width
--- line nav
-vim.keymap.set('n', 'B', '0')
-vim.keymap.set('n', 'E', '$')
-
