4b50e4d73831f33938b5b752fcd25a02c939c408
Jan Herout <jan.herout@gmail.com>
2026-01-21 08:44:20 +0100

    notify-send

--- bin/cyberark.sh
+++ bin/cyberark.sh
@@ -6,8 +6,9 @@ cyberark_rdp_file="$1"
 [ -z "$cyberark_rdp_file" ] && echo "Usage: $0 <rdp file.rdp>" && exit 1
 
 # call xfreerdp with the provided rdp file
+notify-send "xfreerdp spuštěno" "cyberark.sh: xfreerdp: spuštěno na desktopu 3" --icon=dialog-question -t 3000
 xfreerdp "$cyberark_rdp_file"
 
 # remove the file after
-notify-send "Removing: $cyberark_rdp_file" --icon=dialog-information
+notify-send "xfreerdp cleanup" "cyberark.sh: rm $cyberark_rdp_file" --icon=dialog-information
 rm "$cyberark_rdp_file"
